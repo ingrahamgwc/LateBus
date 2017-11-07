777f4d3fa31fe8df84ac55d5c0037d6b74e40453
rmango <morgan-graham@live.com>
2017-11-07 13:59:53 -0800

    testing out vue js. Added 1006 bus page

---
+++ js/vueJsTest.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Some of this data is fake',
+    data: {
+      bus1006: ["1006", 
+      	["Garfield HS LZ 400 23rd AVE", "8:03", "predicted time", "latitude", "longitude"],
+        ["23rd AVE E @ E Aloha St", "8:03", "predicted time", "latitude", "longitude"], 
+        ["35th Ave NE @ NE 65th St", "8:14", "predicted time", "latitude", "longitude"]],
+      bus1007: ["1007", 
+      	["99th St"], 
+        ["109th St"], 
+        ["82nd St"]]
+    }
+  }
+})
