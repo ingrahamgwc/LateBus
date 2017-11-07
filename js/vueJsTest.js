076032ccab241cebe49af4f3fefe7f6e685d00fc
rmango <morgan-graham@live.com>
2017-11-07 14:35:58 -0800

    cleaning up js

--- js/vueJsTest.js
+++ js/vueJsTest.js
@@ -3,13 +3,12 @@ new Vue({
   data: {
     message: 'Some of this data is fake',
     data: {
-      bus1006: ["1006", 
-      	["Garfield HS LZ 400 23rd AVE", "8:03", "predicted time", "latitude", "longitude"],
-        ["23rd AVE E @ E Aloha St", "8:03", "predicted time", "latitude", "longitude"], 
-        ["35th Ave NE @ NE 65th St", "8:14", "predicted time", "latitude", "longitude"]],
-      bus1007: ["1007", 
-      	["99th St"], 
-        ["109th St"], 
+      bus1006: [["Garfield HS LZ 400 23rd AVE", "8:03", "predicted time", "latitude", "longitude"],
+      ["23rd AVE E @ E Aloha St", "8:03", "predicted time", "latitude", "longitude"],
+      ["35th Ave NE @ NE 65th St", "8:14", "predicted time", "latitude", "longitude"]],
+      bus1007: ["1007",
+        ["99th St"],
+        ["109th St"],
         ["82nd St"]]
     }
   }
