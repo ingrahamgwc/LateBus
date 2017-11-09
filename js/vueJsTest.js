4ab537f307c16033668a4c804bac3f9bdc2a8174
rmango <morgan-graham@live.com>
2017-11-08 23:14:42 -0800

    adding bus stop onclick expansions

--- js/vueJsTest.js
+++ js/vueJsTest.js
@@ -1,4 +1,4 @@
-new Vue({
+var app = new Vue({
   el: '#app',
   data: {
     currentRoute: 1006,
@@ -27,5 +27,24 @@ new Vue({
         long: 2.4
       }]
     }
+  },
+  methods: {
+    expand: function (event) {
+      console.log("hi" + event);
+    }
   }
 })
+
+//to make the stop info expand down (thanks w3schools)
+var acc = document.getElementsByClassName("accordion");
+for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
