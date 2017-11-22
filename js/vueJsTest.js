0fa318d8941f57c0a634407444aed824499fbac7
rmango <morgan-graham@live.com>
2017-11-22 15:43:45 -0800

    Adding bus stop data to routing

--- js/vueJsTest.js
+++ js/vueJsTest.js
@@ -1,9 +1,47 @@
-var app = new Vue({  
+var app = new Vue({
   el: '#app',
   data: {
+    //Depends on ?route=number format
     currentRoute: document.location.search.substring(7),
-    bus: ["1003", "1004", "1005", "1006"],
-    route1006: {
+    buses: [{
+      name: "1004",
+      stops: [{
+        address: "NW 85th St @ 17th AVE NW",
+        schedTime: "8:24",
+        predTime: "predicted time",
+        lat: 1.00,
+        long: 2.4
+      },
+      {
+        address: "NW 85th St@ 24th AVE NW",
+        schedTime: "8:25",
+        predTime: "predicted time",
+        lat: 1.00,
+        long: 2.4
+      },
+      {
+        address: "NW 85th St@ 24th AVE NW",
+        schedTime: "8:25",
+        predTime: "predicted time",
+        lat: 1.00,
+        long: 2.4
+      },
+      {
+        address: "24th AVE NW @ NW 96th St",
+        schedTime: "8:27",
+        predTime: "predicted time",
+        lat: 1.00,
+        long: 2.4
+      },
+      {
+        address: "15th AVE NW @ NW 100th St",
+        schedTime: "8:30",
+        predTime: "predicted time",
+        lat: 1.00,
+        long: 2.4
+      }]
+    },
+    {
       name: "1006",
       stops: [{
         address: "Garfield HS LZ 400 23rd AVE",
@@ -26,26 +64,35 @@ var app = new Vue({
         lat: 1.00,
         long: 2.4
       }]
-    }
+    }]
   },
   methods: {
     expand: function (event) {
-      console.log("hi" + event);
-      console.log(this);      
+      /*console.log("hi" + HTMLDivElement);
+      this.style.display = "visible";
+      console.log(document.getElementsByClassName("accordion"));
+      //document.getElementsByClassName("accordion").classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }*/
     }
   }
 })
 
+
 //to make the stop info expand down (thanks w3schools)
 var acc = document.getElementsByClassName("accordion");
 for (var i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
+  acc[i].onclick = function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
     }
+  }
 }
