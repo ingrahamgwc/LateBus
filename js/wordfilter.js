c18602e6f4866b9f600b866e0f11f9bf000b1c6f
Tillandsia <123avakloss@gmail.com>
2018-05-16 15:58:49 -0700

    Update wordfilter.js

--- js/wordfilter.js
+++ js/wordfilter.js
@@ -3,7 +3,7 @@
 
 //var localList = require('./lang.json').words;
 //var baseList = require('badwords-list').array;
-var baselist;
+var baselist = [];
 var Filter = (function() {
   /**
    * Filter constructor.
